1822d9750eb1ea1e757177b57291eefed82ed1f4
What1sLov3RK <72340160+What1sLov3RK@users.noreply.github.com>
2025-10-04 18:42:41 +0100

    Fix

--- GuitarLavk/settings.py
+++ GuitarLavk/settings.py
@@ -143,3 +143,6 @@ LOGIN_REDIRECT_URL = 'home'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = config('SECURE_SSL_REDIRECT', default=False, cast=bool)

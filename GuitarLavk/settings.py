5a938ada44771cbceac689f9be56f473dd55c468
What1sLov3RK <72340160+What1sLov3RK@users.noreply.github.com>
2025-10-04 21:41:56 +0100

    Fix template

--- GuitarLavk/settings.py
+++ GuitarLavk/settings.py
@@ -84,14 +84,16 @@ USE_TZ = True
 CART_SESSION_ID = 'cart'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / "static",
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / "media"
+
 
 CACHES = {
     'default': {

36c36cb6e885f9637d9b8250c676e7e8ff067487
What1sLov3RK <72340160+What1sLov3RK@users.noreply.github.com>
2025-10-04 18:04:34 +0100

    Added Secure proxy ssl header in settings

--- GuitarLavk/settings.py
+++ GuitarLavk/settings.py
@@ -143,3 +143,5 @@ LOGIN_REDIRECT_URL = 'home'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')

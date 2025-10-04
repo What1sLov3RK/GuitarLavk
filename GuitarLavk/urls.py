6de0e27ca7d3b4f20cd9ac0a77cfca362bfcfa38
What1sLov3RK <72340160+What1sLov3RK@users.noreply.github.com>
2025-10-04 21:32:24 +0100

    Fix template

--- GuitarLavk/urls.py
+++ GuitarLavk/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from guitarlavka import views
-from guitarlavka.views import *
+from guitarlavka.views import (
+    Home, Catalogue, Product_view,
+    RegisterUser, LoginUser, Cabinet,
+    OrderView, Cart_view, AddToCart,
+    remove_from_cart, order_success, logout_view
+)
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -16,8 +21,8 @@ urlpatterns = [
     path("cabinet/order/<int:order_id>", OrderView.as_view(), name='order'),
     path('cart/', Cart_view.as_view(), name='cart'),
     path('add-to-cart/<slug:product_slug>/', AddToCart.as_view(), name='add_to_cart'),
-    path('remove-from-cart/<int:cart_item_id>/', views.remove_from_cart, name='remove_from_cart'),
-    path('cart/order_success/', views.order_success, name='order_success'),
-    path('logout/', views.logout_view, name='logout'),
+    path('remove-from-cart/<int:cart_item_id>/', remove_from_cart, name='remove_from_cart'),
+    path('cart/order_success/', order_success, name='order_success'),
+    path('logout/', logout_view, name='logout'),
 ]\
               + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
